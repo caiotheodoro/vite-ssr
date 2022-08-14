8355f0299a380e18eba63ad58990f8f0e65cb62c
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-08-14 19:40:19 -0300

    feat: 🎸 main page  + actions

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import { HomeContainer } from './styles'
+import React, { useEffect } from 'react'
+import { HomeContainer, MainPanel, MainPanelDescription } from './styles'
 import { Banner } from '../../components/Banner'
-
-
+import { motion } from 'framer-motion'
+import { Code, FastForward } from 'phosphor-react'
 export function Page() {
+
+
+  useEffect(() => {
+
+    const taste = document.getElementById('taste')
+    if (taste) {
+      const changeFont = () => {
+        const fonts = ['Roboto', 'Arial ', 'Poppins', 'RHelvetica', 'Tahoma', 'Trebuchet MS', 'Times New Roman', 'Georgia', 'Garamond', 'Courier New']
+        const colors = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#9e9e9e', '#607d8b']
+        const randomFont = fonts[Math.floor(Math.random() * fonts.length)]
+        taste.style.fontFamily = randomFont
+        taste.style.color = colors[Math.floor(Math.random() * colors.length)]
+        setTimeout(changeFont, 500);
+      }
+      changeFont()
+    }
+
+
+  }, []);
+
+
   return (
     <HomeContainer>
-      adsf
+      <MainPanel>
+        <div>
+          Explore your personality
+          <br />
+          and <motion.span
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >expand</motion.span>
+          <br />
+          your music <span id="taste">taste.</span>
+        </div>
+        <MainPanelDescription>
+          discover collaborating with your tastes the best songs for you!
+          <div>
+            <button>
+              <Code size={24} />
+              Behind the scenes
+            </button>
+            <button>
+              Check out some tests
+              <FastForward size={24} />
+            </button>
+          </div>
+        </MainPanelDescription>
+      </MainPanel>
       <Banner />
     </HomeContainer>
   )
--- src/pages/index/styles.ts
+++ src/pages/index/styles.ts
@@ -16,18 +16,80 @@ export const HomeContainer = styled.div`
   
 `
 
-const Spashes = styled.img`
-position: absolute;
-width: 300px; 
-height: auto;
+
+export const MainPanel = styled.div`
+    width: 100%;
+    display: flex;
+    margin: 100px auto;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 4rem;
+    font-weight: 700;
+    color: ${({ theme }) => theme['base-card']};
+
+    span {
+        display: contents;
+        flex-direction: row;
+
+    }
+
 `
 
-export const Spashes1 = styled(Spashes)`
-right: -130px;
-bottom: -100px;
-`
+export const MainPanelDescription = styled.div`
+    width: 100%;
+    display: flex;
+    margin: 50px auto;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 1.25rem;
+    gap: 2rem;
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 2rem;
+        button {
+            display: flex;
+            gap: 0.5rem;
+            justify-content: center;
+            align-items: center;
+            padding:0.5rem  2rem;
+            transition: all 0.3s ease-in-out;
+            height: 50px;
+            border-radius: 10px;
+            border: none;
+            background: linear-gradient(-45deg, #0D0A0B, #4b2995, #4b2995);
+            animation: gradient 15s ease infinite;
+            background-size: 400% 400%;
 
-export const Spashes2 = styled(Spashes)`
-left: -242px;
-top: -6px;
+            
+        }
+
+        button:hover {
+            border: 2px solid ${({ theme }) => theme['base-card']};
+            transform: scale(1.1);
+        }
+
+        button + button {
+            background: linear-gradient(to right, #1db954, #4b2995);
+        }
+
+       
+
+        @keyframes gradient {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+}
+
+    }
+
 `
